a06a6d0e675379bddf6a1ed3918d5cff5049b919
Ahsan Mumtaz <ahsanmumtaz008976@gmail.com>
2023-03-16 22:58:11 +0500

    Update navigation.cy.js

--- routing-app/cypress/e2e/navigation.cy.js
+++ routing-app/cypress/e2e/navigation.cy.js
@@ -11,3 +11,21 @@ describe('navigation', () => {
     cy.location('pathname').should('eq', '/');
   });
 });
+
+import * as XLSX from 'xlsx';
+
+describe('Example Test', () => {
+  it('loads and parses xlsx file', () => {
+    cy.fixture('example.xlsx', {parse: false}).then((fileContent) => {
+      // Parse the file content using xlsx library
+      const workbook = XLSX.read(fileContent, {type: 'binary'});
+      const sheet = workbook.Sheets['Sheet1'];
+      const data = XLSX.utils.sheet_to_json(sheet);
+
+      // Do something with the parsed data
+      expect(data.length).to.equal(2);
+      expect(data[0].name).to.equal('John Doe');
+      expect(data[0].email).to.equal('johndoe@example.com');
+    })
+  })
+})
